e36b7c1a209fd02e5e5984faab122f779c646913
nedinator <imnedinator@gmail.com>
2024-01-03 21:09:52 -0600

    mostly testing git config & some prep for pawnmenu

---
+++ scripts/Pawn/PawnMenu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+func _on_chop_pressed():
+	pass # Replace with function body.
+
+func _on_mine_pressed():
+	pass # Replace with function body.
+
+func _on_walk_pressed():
+	pass # Replace with function body.
+
+func _on_repair_pressed():
+	pass # Replace with function body.
